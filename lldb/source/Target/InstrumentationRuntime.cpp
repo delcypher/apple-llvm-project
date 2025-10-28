95480a39700ea38b4b938f017657da56efcc1dbe
Dan Liew <dan@su-root.co.uk>
2025-10-28 08:54:22 -0700

    [LLDB][BoundsSafety] Add `InstrumentationRuntime::MatchAllModules`
    This adds a virtual method that allows `InstrumentationRuntime`
    sub-classes to match against all modules rather than just a library
    that matches a particular regex. When the implementation returns
    true `GetPatternForRuntimeLibrary()` is ignored and all modules
    are iterated over. The default implementation returns false which
    was the previous behavior which uses `GetPatternForRuntimeLibrary()`
    to only match a particular runtime library.
    
    The intended use case here is for implementing an
    `InstrumentationRuntime` where the runtime library of interest can have
    multiple implementations and whose name is not known ahead of time.
    
    The concrete use case here is for a `InstrumentationRuntime` plugin
    for implementations of the `-fbounds-safety` soft-trap runtime which
    can have multiple different implementations.
    
    An alternative to this would be for the `GetPatternForRuntimeLibrary()`
    function to return a regex that matches everything. While that
    technically works it seems wasteful to execute a regex for every loop
    iteration.
    
    rdar://FIXME

--- lldb/source/Target/InstrumentationRuntime.cpp
+++ lldb/source/Target/InstrumentationRuntime.cpp
@@ -55,8 +55,8 @@ void InstrumentationRuntime::ModulesDidLoad(
       return IterationAction::Continue;
 
     const RegularExpression &runtime_regex = GetPatternForRuntimeLibrary();
-    if (runtime_regex.Execute(file_spec.GetFilename().GetCString()) ||
-        module_sp->IsExecutable()) {
+    if (MatchAllModules() || module_sp->IsExecutable() ||
+        runtime_regex.Execute(file_spec.GetFilename().GetCString())) {
       if (CheckIfRuntimeIsValid(module_sp)) {
         SetRuntimeModuleSP(module_sp);
         Activate();
